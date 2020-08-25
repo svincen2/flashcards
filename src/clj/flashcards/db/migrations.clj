0d9fd96719cbc02a0a9c2a8df98f2f357bb84aa0
Sean Vincent <seanmv2@gmail.com>
2020-08-24 19:38:15 -0600

    Utility fn to make up+down migration files

--- src/clj/flashcards/db/migrations.clj
+++ src/clj/flashcards/db/migrations.clj
@@ -1,7 +1,11 @@
 (ns flashcards.db.migrations
   (:require [flashcards.db :as db]
             [ragtime.jdbc :as jdbc]
-            [ragtime.repl :as repl]))
+            [ragtime.repl :as repl]
+            [clojure.java.io :as io]
+            [clojure.string :as string])
+  (:import [java.time LocalDate]
+           [java.time.format DateTimeFormatter]))
 
 (defn config
   []
@@ -15,3 +19,13 @@
 (defn rollback
   []
   (repl/rollback (config)))
+
+(defn create-migration
+  [table description]
+  (let [today (.format (LocalDate/now) DateTimeFormatter/BASIC_ISO_DATE)
+        name (string/split description #"\s+")
+        [up down] (map (fn [type]
+                         (str today \_ table \_ (string/join \- name) \. type ".sql"))
+                       ["up" "down"])]
+    (spit (str "resources/migrations/" up) (str "-- " table ": " description))
+    (spit (str "resources/migrations/" down) (str "-- " table ": " description))))
